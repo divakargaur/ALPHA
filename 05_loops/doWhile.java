fd077100e8d9daa6ef364637c5ffe7de2c02844c
Divakar Gaur <divakargaur270@gmail.com>
2023-10-21 22:25:12 +0530

    21-10-2023

---
+++ 05_loops/doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("RADHE RADHE");
+            count++;
+        } while (count<=10);
+    }
+}
