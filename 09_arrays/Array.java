88947c5abf3e49ec8b8b23d121f384bf042bc7c7
Divakar Gaur <divakargaur270@gmail.com>
2024-07-19 16:42:25 +0530

    19 july_1

---
+++ 09_arrays/Array.java
@@ -0,0 +1,21 @@
+// - array is a list of elements of the same type placed in a contagious memory location.
+
+// - java char takes up 2 bytes in memory.
+
+// operation in arrays - create, input, output & update
+
+
+
+// creating an array
+
+public class Array {
+    public static void main(String[] args) {
+        // int marks[] = new int[50];
+
+        // int numbers[] = {1, 2, 3};
+
+        // int moreNumbers[] = {4, 5, 6};
+
+        // String fruits[] = {"apple", "mango", "orange"};
+    }
+}
---
+++ 09_arrays/newArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class newArray {
+    public static void main(String[] args) {
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("length of array: " + marks.length); // length of the array
+
+        marks[0] = sc.nextInt(); // phy
+        marks[1] = sc.nextInt(); // chem
+        marks[2] = sc.nextInt(); // math
+
+        System.out.println("phy: " + marks[0]);
+        System.out.println("chem: " + marks[1]);
+        System.out.println("math: " + marks[2]);
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("percentage: " + percentage + "%");
+    }
+}
